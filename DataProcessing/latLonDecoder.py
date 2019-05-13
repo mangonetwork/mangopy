556600de5614ed79802aa86bcfb62f23ba748c80
Leslie Lamarche <leslie.lamarche@sri.com>
2019-05-13 08:28:49 -0700

    Corrected numpy import in latLonDecoder.py.

--- DataProcessing/latLonDecoder.py
+++ DataProcessing/latLonDecoder.py
@@ -12,7 +12,6 @@
 #########################################################
 #### takes newi and newj .csvs and converts them to lat long ####
 #### this code is based on undoing a portion of what pixelToLatLon did ####
-from numpy import *
 import numpy as np
 
 #### define image dimensions ####
@@ -22,20 +21,17 @@ newImageHeight = 500
 newImageWidth = 500
 
 #### initialize arrays ####
-IMatrix = empty(([ImageHeight, ImageWidth]))         # creating an empty array with the same dimensions as the raw image
-JMatrix = empty(([ImageHeight, ImageWidth]))         # creating an empty array with the same dimensions as the raw image
-IMatrix[:] = NAN          # Setting the latitude array to NANs
-JMatrix[:] = NAN         # Setting the longitude array to NANs
+IMatrix = np.empty(([ImageHeight, ImageWidth]))         # creating an empty array with the same dimensions as the raw image
+JMatrix = np.empty(([ImageHeight, ImageWidth]))         # creating an empty array with the same dimensions as the raw image
+IMatrix[:] = np.nan          # Setting the latitude array to NANs
+JMatrix[:] = np.nan         # Setting the longitude array to NANs
 
 
 #### load data ####
-IMatrix= genfromtxt('newI.csv', dtype=float, delimiter=',')
-JMatrix = genfromtxt('newJ.csv', dtype=float, delimiter=',')
-imageLatLong = genfromtxt('imageLatLong.csv', dtype=float, delimiter=',')
+IMatrix = np.genfromtxt('newI.csv', dtype=float, delimiter=',')
+JMatrix = np.genfromtxt('newJ.csv', dtype=float, delimiter=',')
+imageLatLong = np.genfromtxt('imageLatLong.csv', dtype=float, delimiter=',')
 
-# Lat = genfromtxt('/Users/e30737/Desktop/Projects/InGeO/MIVIT/MANGO/Data/HatCreek_Latitudes_old.csv', dtype=float, delimiter=',')
-# Lon = genfromtxt('/Users/e30737/Desktop/Projects/InGeO/MIVIT/MANGO/Data/HatCreek_Longitudes_old.csv',dtype=float, delimiter=',')
-# imageLatLong = np.array([[np.nanmin(Lat.ravel()),np.nanmin(Lon.ravel())],[np.nanmax(Lat.ravel()),np.nanmax(Lon.ravel())]])
 
 ## get these values from the imageLatLon.csv
 #### set bounds ####
@@ -45,25 +41,25 @@ latRange          = latBounds[1]-latBounds[0]                   #currently not u
 lonBounds         = imageLatLong[:,1]                           #min and max lon values from ImageLatLon.csv
 lonRange          = lonBounds[1]-lonBounds[0]                   #span of longitude values (length between max and min)
 
-scaleBounds       = log(tan(radians(latBounds)*0.5 + pi/4))     #two value list of scaled min and max [(scalemin), (scalemax)]
+scaleBounds       = np.log(np.tan(np.radians(latBounds)*0.5 + pi/4))     #two value list of scaled min and max [(scalemin), (scalemax)]
 scaleRange        = scaleBounds[1]-scaleBounds[0]               #span of scale values (length between min and max)
 
 
 #### initialize arrays ####
-latMatrix = empty(([newImageHeight, newImageWidth]))          # creating an empty array with the same dimensions as the new image
-lonMatrix = empty(([newImageHeight, newImageWidth]))         # creating an empty array with the same dimensions as the new image   
-latMatrix[:] = NAN          # Setting the lat array to NANs
-lonMatrix[:] = NAN         # Setting the lon array to NANs
+latMatrix = np.empty(([newImageHeight, newImageWidth]))          # creating an empty array with the same dimensions as the new image
+lonMatrix = np.empty(([newImageHeight, newImageWidth]))         # creating an empty array with the same dimensions as the new image   
+latMatrix[:] = np.nan          # Setting the lat array to NANs
+lonMatrix[:] = np.nan         # Setting the lon array to NANs
 
 #### calculate lat and lon from j and i ####
 for j in range(ImageHeight):
     for i in range(ImageWidth):
         Icell = IMatrix[j][i]
         Jcell = JMatrix[j][i]
-        if (not(isnan(Icell)) and not(isnan(Jcell))):
+        if (not(np.isnan(Icell)) and not(np.isnan(Jcell))):
             pixLon = lonBounds[0] + ((Icell*lonRange)/np.float64(newImageWidth -1)) #perfect!
             scaleJ = scaleBounds[0] + (scaleRange)*((np.float64(newImageHeight-1)-Jcell)/(newImageHeight-1))
-            pixLat = degrees(2*(arctan(exp(scaleJ))-(pi/4))) 
+            pixLat = np.degrees(2*(np.arctan(np.exp(scaleJ))-(np.pi/4))) 
             #latMatrix[j-15,i-40] = pixLat #uncomment for Pescadero High School
             #lonMatrix[j-15,i-40] = pixLon #uncomment for Pescadero High School
             # latMatrix[j,i] = pixLat
@@ -72,11 +68,11 @@ for j in range(ImageHeight):
             lonMatrix[int(Jcell),int(Icell)] = pixLon
             
 #### calculate the nonnan SW and NE corners of each location
-southWestLatLon = [nanmin(latMatrix), nanmin(lonMatrix)]
-northEastLatLon = [nanmax(latMatrix), nanmax(lonMatrix)]
-imageLatitudesLongitudes = array((vstack((southWestLatLon, northEastLatLon))))  # creates an array stacked for writing to a CSV
+southWestLatLon = [np.nanmin(latMatrix), np.nanmin(lonMatrix)]
+northEastLatLon = [np.nanmax(latMatrix), np.nanmax(lonMatrix)]
+imageLatitudesLongitudes = np.array((np.vstack((southWestLatLon, northEastLatLon))))  # creates an array stacked for writing to a CSV
 
 #### save the matricis to files in their respctive directories ####
-savetxt('Latitudes.csv', latMatrix, delimiter=',')
-savetxt('Longitudes.csv', lonMatrix, delimiter=',')
-savetxt('ImageLatLongnew.csv', imageLatitudesLongitudes, delimiter=',')
+np.savetxt('Latitudes.csv', latMatrix, delimiter=',')
+np.savetxt('Longitudes.csv', lonMatrix, delimiter=',')
+np.savetxt('ImageLatLongnew.csv', imageLatitudesLongitudes, delimiter=',')
