be93c5202f31d8ed01a1b86b79b3f829c63a4c49
Leslie Lamarche <leslie.lamarche@sri.com>
2021-07-06 10:59:51 -0700

    Hardcode requirements in setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
 import os
 
-# Get the package requirements
-REQSFILE = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-with open(REQSFILE, 'r') as f:
-    REQUIREMENTS = f.readlines()
-REQUIREMENTS = '\n'.join(REQUIREMENTS)
+# # Get the package requirements
+# REQSFILE = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+# with open(REQSFILE, 'r') as f:
+#     REQUIREMENTS = f.readlines()
+# REQUIREMENTS = '\n'.join(REQUIREMENTS)
+REQUIREMENTS = [
+    'scipy >= 1.2',
+    'h5py >= 2.9',
+    'matplotlib >= 2.2.4'
+]
 
-setup(name='mangopy',
+setup(name='mangopy_test',
       version='0.1',
       description='Software for MANGO ASI network',
+      author = 'P. Venkatraman, L. Lamarche, A. Bhatt',
       license='GPLv3',
       packages=['mangopy'],
       install_requires=REQUIREMENTS,
